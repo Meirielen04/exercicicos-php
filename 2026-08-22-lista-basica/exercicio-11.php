1ce383f1ac07cb71b646d0b97d095ec5a0008e29
Meirielen <meirielendasilva4@gmail.com>
2026-08-27 18:44:31 -0300

    ex 11

--- 2026-08-22-lista-basica/exercicio-11.php
+++ 2026-08-22-lista-basica/exercicio-11.php
@@ -2,11 +2,16 @@
 // Desconto por Perfil de Cliente (match): Crie um script onde uma variável armazena o tipo de cliente (Bronze, Prata, Ouro, VIP) 
 // e o valor de uma compra. O match deve retornar a porcentagem de desconto a ser aplicada no total. 
 
-$clienteTier = "VIP";
+$valorCompra = "VIP = 10.000";
 
-$valores = match ($clienteTier) {
-     => ,
-     => ,
-}
+$clienteTier = match ($valorCompra) {
+    "VIP = 10.000" => 0.6,
+    "Ouro = 7.000" => 0.4,
+    "Prata = 3.500" => 0.3,
+    "Bronze = 1.500" => 0.2,
+    "Default" => 0,
+};
+
+echo $clienteTier;
 
 ?>
