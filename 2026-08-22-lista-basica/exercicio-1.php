2c058b3fdf96d623255110f800e8376097f7f62e
Meirielen <meirielendasilva4@gmail.com>
2026-08-27 15:49:24 +0000

    resolução da calculadora com switch

--- 2026-08-22-lista-basica/exercicio-1.php
+++ 2026-08-22-lista-basica/exercicio-1.php
@@ -1,1 +1,27 @@
-<!-- 1.	Calculadora Básica (switch): Crie um script que receba dois números e um operador (+, -, *, /). Utilize a estrutura switch para realizar a operação matemática correspondente e exibir o resultado -->
+<?php
+// 1.	Calculadora Básica (switch): Crie um script que receba dois números e um operador (+, -, *, /). 
+// Utilize a estrutura switch para realizar a operação matemática correspondente e exibir o resultado
+
+$n1 = 5;
+$n2 = 7;
+
+$operador = "-";
+
+switch($operador) {
+    case "+":
+        echo "Resultado: " . ($n1 + $n2) . "\n";
+        break;
+    case "-":
+        echo "Resultado: " . ($n1 - $n2) . "\n";
+        break;
+    case "*":
+        echo "Resultado: " . ($n1 * $n2) . "\n";
+        break;
+    case "/":
+        echo "Resultado: " . ($n1 / $n2) . "\n";
+        break;
+    default:
+        echo "Escolha um operador válido +, -, /, *";
+}
+
+?>
