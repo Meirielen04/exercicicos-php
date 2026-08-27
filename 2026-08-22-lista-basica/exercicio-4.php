014cb5df4c5ef0132a45e696ad7cc942fd583e5d
Meirielen <meirielendasilva4@gmail.com>
2026-08-27 15:06:50 -0300

    ex de locação de luxo e ddd

--- 2026-08-22-lista-basica/exercicio-4.php
+++ 2026-08-22-lista-basica/exercicio-4.php
@@ -1,1 +1,17 @@
-<!-- Aluguel de Carro Executivo (OR): Crie um script que receba a idade e a renda mensal de um cliente. Utilize a estrutura if/else com o operador lógico || para verificar se o cliente possui idade igual ou superior a 25 anos ou comprovação de renda acima de R$ 7.000,00, exibindo se a locação da categoria luxo foi autorizada. -->
+<?php 
+//Aluguel de Carro Executivo (OR): Crie um script que receba a idade e a renda mensal de um cliente. 
+// Utilize a estrutura if/else com o operador lógico || para verificar se o cliente possui idade 
+// igual ou superior a 25 anos ou comprovação de renda acima de R$ 7.000,00, exibindo se a locação 
+// da categoria luxo foi autorizada. -->
+
+$idadeCliente = 40;
+$rendaMensal = 5000;
+
+if ($idadeCliente >= 25 || $rendaMensal > 7000) {
+    echo "Parabéns, sua locação de luxo foi autorizada";
+} else {
+    echo "Idade e renda não possui os requisitos para locação de luxo";
+}
+
+
+?>
--- 2026-08-22-lista-basica/exercicio-5.php
+++ 2026-08-22-lista-basica/exercicio-5.php
@@ -1,1 +1,16 @@
-<!-- Classificador de DDD (match): Crie uma variável com um DDD (ex: 11, 16, 19, 21). Usando a expressão match, retorne a região correspondente (ex: 19 → "Campinas/Região", 16 → "Ribeirão Preto/São Carlos/Porto Ferreira", etc.). -->
+<?php
+// Classificador de DDD (match): Crie uma variável com um DDD (ex: 11, 16, 19, 21). 
+// Usando a expressão match, retorne a região correspondente (ex: 19 → "Campinas/Região", 16 → "Ribeirão Preto/São Carlos/Porto Ferreira", etc.)
+
+$ddd = 19;
+
+$regiao = match($ddd) {
+    19 => "Porto Ferreira",
+    16 => "Ribeirão preto",
+    11 => "São Paulo",
+    default => "DDD inválido",
+};
+
+echo $regiao
+
+?>
