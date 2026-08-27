1fcbad67404134f2491ded66fba8d6fb33f6ef36
Meirielen <meirielendasilva4@gmail.com>
2026-08-27 15:27:07 -0300

    tabuada e reajuste salarial

--- 2026-08-22-lista-basica/exercicio-6.php
+++ 2026-08-22-lista-basica/exercicio-6.php
@@ -1,1 +1,10 @@
-<!-- Gerador de Tabuada Completa (for): Crie um algoritmo utilizando laços for para imprimir a tabuada do 3. -->
+<?php
+// Gerador de Tabuada Completa (for): Crie um algoritmo utilizando laços for para imprimir a tabuada do 3.
+
+for($tab = 0; $tab <= 30; $tab = $tab + 3) {
+
+    echo $tab;
+    echo "\n";
+}
+
+?>
--- 2026-08-22-lista-basica/exercicio-7.php
+++ 2026-08-22-lista-basica/exercicio-7.php
@@ -1,1 +1,12 @@
-<!-- Reajuste Salarial por Produtividade (Aritmético): Crie um script que receba o salário base de um funcionário e a porcentagem de bônus por metas atingidas. Calcule o valor correspondente ao aumento percentual, some-o ao salário base e exiba o novo salário final com o reajuste aplicado. -->
+<?php
+// <!-- Reajuste Salarial por Produtividade (Aritmético): Crie um script que receba o salário base de um funcionário 
+//  e a porcentagem de bônus por metas atingidas. Calcule o valor correspondente ao aumento percentual, some-o ao 
+//  salário base e exiba o novo salário final com o reajuste aplicado. -->
+
+$salarioBase = 3000;
+$bonusPorcent = 0.2;
+$precoBonus = $salarioBase * $bonusPorcent;
+
+echo "Salário base + bonus será de: " . $salarioBase + $precoBonus;
+
+?>
