e9e12bed8b126eadff9ba4642d65ea273f8fabce
Meirielen <meirielendasilva4@gmail.com>
2026-09-09 15:23:27 -0300

    inclusaão de mais exercicios

---
+++ 2026-09-05-exercicios-complementares/consumoCombustivel/script.php
@@ -0,0 +1,6 @@
+<?php
+// Cálculo de Consumo de Combustível (Aritmético): Crie um script que receba a distância total percorrida (em km) e o total de combustível gasto (em litros). Realize a divisão entre esses valores para calcular o consumo médio do veículo e exiba o resultado em km/l. 2
+
+
+
+?>
