19a1b4c18ed1b140e9fd178a2c511db1f8888aa2
Meirielen <meirielendasilva4@gmail.com>
2026-09-10 15:40:17 -0300

    ex 2 - array

--- 2026-09-05-arrays/exercicio-2.php
+++ 2026-09-05-arrays/exercicio-2.php
@@ -1,4 +1,6 @@
 <?php
 /*Crie um array simples com três meios de transporte. Exiba uma frase informando qual meio de transporte você mais utiliza, acessando um elemento do array pela posição.*/
 
+$tipoTransporte = ["ônibus","carro","moto"];
+echo "O transporte que mais utilizo no dia a dia é " . $tipoTransporte[0];
 ?>
