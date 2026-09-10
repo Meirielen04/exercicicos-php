6430333881555fc44737ab2c2d1cada167b19f6f
Meirielen <meirielendasilva4@gmail.com>
2026-09-10 19:54:18 -0300

    ex 7 - array

--- 2026-09-05-arrays/exercicio-7.php
+++ 2026-09-05-arrays/exercicio-7.php
@@ -1,4 +1,13 @@
 <?php
 /*Crie um array associativo com códigos e preços de três produtos. Use `foreach` para exibir o código e o preço de cada produto.*/
 
+$produtos =[
+    "SKU1001" => 2.50,
+    "SKU1002" => 5,
+    "SKU1003" => 7.50
+];
+
+foreach ($produtos as $cod => $preco) {
+    echo "Codigo do produto {$cod} tem o preço de {$preco} \n";
+}
 ?>
