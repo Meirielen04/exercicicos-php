9f70c647d0b1960266be8662ede576a449ecda0e
Meirielen <meirielendasilva4@gmail.com>
2026-09-10 15:54:15 -0300

    ex 3 - array

--- 2026-09-05-arrays/exercicio-3.php
+++ 2026-09-05-arrays/exercicio-3.php
@@ -1,4 +1,11 @@
 <?php
 /*Crie um array com quatro temperaturas. Use `for` e `count` para percorrer o array e exibir cada temperatura com a sua posição numerada.*/
 
+$temperatura = [35,3,15,22];
+
+echo count ($temperatura) . "\n";
+
+for ($i = 0; $i < count($temperatura); $i++){
+    echo "temperatura: $temperatura[$i]" . "\n";
+}
 ?>
