c3f4c7381ec4c22c0e6550d37c233450e024b4b4
Meirielen <meirielendasilva4@gmail.com>
2026-09-10 20:07:12 -0300

    ex 8 - array

--- 2026-09-05-arrays/exercicio-8.php
+++ 2026-09-05-arrays/exercicio-8.php
@@ -1,4 +1,12 @@
 <?php
 /*Crie uma matriz 3x3 representando um mapa de assentos, usando `L` para assento livre e `O` para assento ocupado. Exiba o valor de uma posição específica da matriz.*/
 
+$mapaAssentos = [
+    ["l","o","l"],
+    ["o","o","l"],
+    ["l","l","o"]
+];
+
+echo "O primeiro assento da fileira {$mapaAssentos[0][0]} está livre";
+
 ?>
