1d5fab89ff08ddc1ab2a70e7fa2380db21f96cb5
Meirielen <meirielendasilva4@gmail.com>
2026-09-10 18:38:23 -0300

    ex 6 - array

--- 2026-09-05-arrays/exercicio-6.php
+++ 2026-09-05-arrays/exercicio-6.php
@@ -1,4 +1,14 @@
 <?php
 /*Crie um array associativo com códigos e nomes de três disciplinas. Use `foreach` para exibir somente os nomes das disciplinas.*/
 
+$curso =[
+    "001" => "Tecnicas de Programação",
+    "002" => "Desenvolvimento Web",
+    "003" => "Interação Humano Computador"
+];
+
+foreach ($curso as $cod => $disciplinas) {
+    echo "{$disciplinas} faz parte do curso de Desenvolvimento de Software Multiplataforma" . "\n";
+}
+
 ?>
