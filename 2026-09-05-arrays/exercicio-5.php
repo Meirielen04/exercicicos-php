8e639819c6febfc74288dab68a967fa4f95db636
Meirielen <meirielendasilva4@gmail.com>
2026-09-10 18:25:07 -0300

    ex 5 - array

--- 2026-09-05-arrays/exercicio-5.php
+++ 2026-09-05-arrays/exercicio-5.php
@@ -1,4 +1,13 @@
 <?php
 /*Crie um array associativo com informações de um livro: título, autor, ano e disponibilidade. Exiba o título e o autor do livro usando suas chaves.*/
 
+$livro = [
+    "titulo" => "Harry Potter e a Pedra Filosofal",
+    "autor" => "J. K. Rowling",
+    "ano" => 1997,
+    "disponibilidade => true"
+];
+
+echo $livro['titulo'] . " é um livro famoso de " . $livro['autor'];
+
 ?>
