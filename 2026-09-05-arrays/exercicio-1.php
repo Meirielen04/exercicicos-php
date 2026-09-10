a7b5624463b0be0be518aecf26bdb35e43fea1b0
Meirielen <meirielendasilva4@gmail.com>
2026-09-10 15:35:58 -0300

    ex 1 - array

--- 2026-09-05-arrays/exercicio-1.php
+++ 2026-09-05-arrays/exercicio-1.php
@@ -1,4 +1,7 @@
 <?php
 /*Crie um array simples com três cidades. Exiba uma frase informando qual é a sua cidade favorita, acessando um elemento do array pela posição.*/
 
+$cidade = ["Porto Ferreira","Leme", "Pirassununga"];
+echo "Minha cidade favorita é " . $cidade[0] . " porque é onde nasci e tenho orgulho de ser a capital da ceramica e decoração";
+
 ?>
