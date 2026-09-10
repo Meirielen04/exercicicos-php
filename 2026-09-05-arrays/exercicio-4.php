d8b43285683c4e7459ee2ef03257ffea887f1b7d
Meirielen <meirielendasilva4@gmail.com>
2026-09-10 18:05:14 -0300

    ex 4 - array

--- 2026-09-05-arrays/exercicio-4.php
+++ 2026-09-05-arrays/exercicio-4.php
@@ -1,4 +1,12 @@
 <?php
 /*Crie um array com quatro nomes de alunos. Use `for` e `count` para percorrer o array e exibir uma mensagem de boas-vindas para cada aluno.*/
 
+$nomeAlunos = ["Leandro","Ivo","Renata","Wellington"];
+
+count ($nomeAlunos);
+
+for ($i = 0; $i < count($nomeAlunos); $i++){
+    echo "Boas-vindas: $nomeAlunos[$i]" . "\n";
+}
+
 ?>
