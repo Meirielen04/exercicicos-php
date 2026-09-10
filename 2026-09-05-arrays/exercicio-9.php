605af2b74a38ad4c5e108e9ea53511c01d8e402c
Meirielen <meirielendasilva4@gmail.com>
2026-09-10 20:20:32 -0300

    ex 9 - array

--- 2026-09-05-arrays/exercicio-9.php
+++ 2026-09-05-arrays/exercicio-9.php
@@ -1,4 +1,11 @@
 <?php
 /*Crie uma matriz com duas linhas e três colunas contendo notas de duas avaliações de três alunos. Exiba a nota de um aluno em uma avaliação específica usando linha e coluna.*/
 
+$avaliacoes = [
+    [4,6,7],
+    [8,9,5]
+];
+
+echo "O segundo aluno obteu {$avaliacoes[0][1]} na prova de matemática";
+
 ?>
